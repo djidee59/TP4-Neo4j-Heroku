6660666ef549d91ea2fa6a44ff1d126c841520a3
djidee59 <juli1.2vilr@gmail.com>
2017-09-28 12:05:52 +0200

    a
    a

--- app.js
+++ app.js
@@ -5,6 +5,25 @@ const PORT = process.env.PORT || 3000;
 
 var app = express();
 
+// NEO4J
+var neo4j = require('neo4j');
+var db = new neo4j.GraphDatabase(process.env['GRAPHENEDB_URL']);
+
+// TEST NEO4J
+db.cypher({
+    query: 'CREATE (n:Person {name: {personName}}) RETURN n',
+    params: {
+        personName: 'Bob'
+    }
+}, function(err, results){
+    var result = results[0];
+    if (err) {
+        console.error('Error saving new node to database:', err);
+    } else {
+        console.log('Node saved to database with id:', result['n']['_id']);
+    }
+});
+
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 
